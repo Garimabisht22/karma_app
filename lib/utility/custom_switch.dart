f13571e92d5282c81838617c1e1658656016903f
Garima Bisht <bisht.garima01@gmail.com>
2021-08-27 15:42:36 +0530

    made custom switch

---
+++ lib/utility/custom_switch.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomSwitch extends StatefulWidget {
+  @override
+  _CustomSwitchState createState() => _CustomSwitchState();
+}
+
+class _CustomSwitchState extends State<CustomSwitch>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+  bool notifications = true;
+
+  double value = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _animationController = AnimationController(
+        vsync: this,
+        duration: Duration(milliseconds: 80),
+        value: notifications ? 1 : 0);
+    _animation =
+        CurvedAnimation(parent: _animationController, curve: Curves.easeInOut);
+    _animationController.addListener(() {
+      setState(() {
+        value = _animation.value;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Stack(
+        children: [
+          Container(
+            width: 75,
+            height: 30,
+            child:notifications? Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18.0,vertical: 6),
+              child: Text("ON",style: TextStyle(color: Colors.white,fontFamily: "OpenSans"),),
+            ):Text(""),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(6)),
+                color: Color(0xffFF8400)),
+          ),
+          Transform.translate(
+            offset: Offset(50 * value, 0),
+            child: Container(
+              height: 29,
+              width: 25,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(6)),
+                color: Color(0xe6de700d)
+              ),
+            ),
+          ),
+          Transform.translate(offset: Offset(50 * value, 0),
+            child: Container(
+            height: 29,
+            width: 25,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(6)),
+                color: Color(0xe6de700d)
+            ),
+          ),)
+        ],
+      ),
+      onTap: () {
+        if (_animationController.isCompleted) {
+          _animationController.reverse();
+        } else
+          _animationController.forward();
+        notifications = !notifications;
+      },
+    );
+  }
+}
