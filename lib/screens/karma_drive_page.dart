d41fec534924e52f810230406c8de21b9cea08b3
Garima Bisht <bisht.garima01@gmail.com>
2021-08-27 15:42:52 +0530

    build karma drive page

--- lib/screens/karma_drive_page.dart
+++ lib/screens/karma_drive_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:karma_app/screens/login_screen.dart';
+import 'package:karma_app/screens/profile_page.dart';
+import 'package:karma_app/utility/drive_widget.dart';
 
 class KarmaDrivePage extends StatefulWidget {
   static const String id = 'karma_drive_page';
@@ -9,20 +12,125 @@ class KarmaDrivePage extends StatefulWidget {
 }
 
 class _KarmaDrivePageState extends State<KarmaDrivePage> {
+  String searchBarHint = "Search here...";
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       backgroundColor: Color(0xffFF8400),
       body: Column(
         children: [
           Container(
-            child: Row(),
+            margin: EdgeInsets.symmetric(horizontal: 30,vertical: 35),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          GestureDetector(
+                            child: CircleAvatar(
+                              backgroundImage:
+                                  AssetImage('assets/images/profile_image.png'),
+                              radius: 30,
+                            ),
+                            onTap: () {
+                              Navigator.pushNamed(context, ProfilePage.id);
+                            },
+                          ),
+                          // Card(
+                          //   child: Padding(
+                          //     padding: const EdgeInsets.symmetric(
+                          //         horizontal: 8.0, vertical: 2),
+                          //     child: Text(
+                          //       "Profile",
+                          //       style: TextStyle(
+                          //           color: Color(0xffFF8400),
+                          //           fontFamily: "OpenSans",
+                          //           fontWeight: FontWeight.w700),
+                          //     ),
+                          //   ),
+                          // )
+                        ],
+                      ),
+                      Text(
+                        "Karma Drives",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontFamily: "OpenSans"),
+                      ),
+                      OutlinedButton(
+                          onPressed: () {
+                            Navigator.pushReplacementNamed(context, LoginScreen.id);
+                          },
+                          style: OutlinedButton.styleFrom(
+                              backgroundColor: Colors.white),
+                          child: Text(
+                            "LogOut",
+                            style: TextStyle(color: Colors.black),
+                          ))
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(8, 15, 8, 0),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30)),
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(25.0, 0, 5, 0),
+                      child: Row(
+                        //  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                              child: TextField(
+                            // textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                                hintText: searchBarHint,
+                                border: InputBorder.none,
+                                hintStyle: TextStyle(
+                                    color: Color(0xff989898), fontSize: 16)),
+                          )),
+                          CircleAvatar(
+                            child: Icon(
+                              Icons.search_outlined,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                            backgroundColor: Colors.black,
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
           Container(
+            height: height/1.37,
+            width: double.maxFinite,
             decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(35),
+                topRight: Radius.circular(35)
+              ),
               color: Colors.white,
-
             ),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 15.0),
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) {
+                 return DriveWidget();
+                }
+              ),
+            )
           ),
         ],
       ),
---
+++ lib/utility/drive_widget.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'custom_button.dart';
+
+class DriveWidget extends StatefulWidget {
+  @override
+  _DriveWidgetState createState() => _DriveWidgetState();
+}
+
+class _DriveWidgetState extends State<DriveWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+      height: width / 1.4,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          image: DecorationImage(
+              image: AssetImage("assets/images/background.png"),
+              fit: BoxFit.cover,
+              colorFilter:
+                  ColorFilter.mode(Colors.white, BlendMode.difference))),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text("9,523",
+                    style: TextStyle(
+                        fontSize: 22,
+                        fontFamily: "OpenSans",
+                        color: Colors.white,
+                    fontWeight: FontWeight.w500))
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  "JOINED",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontStyle: FontStyle.italic,
+                    fontFamily: "OpenSans",
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+            Expanded(
+              child: SizedBox(
+
+              ),
+            ),
+            Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text("CATEGORY",
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontStyle: FontStyle.italic,
+                          fontFamily: "OpenSans",
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700)),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "SPIRITUAL ENVIRONMENT",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: "OpenSans",
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.access_time_filled,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text("05:00 AM | 07 DEC 2019",
+                          style: TextStyle(
+                              fontSize: 15,
+                             // fontStyle: FontStyle.italic,
+                              fontFamily: "OpenSans",
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700))
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      CustomButton(
+                        width: 120,
+                        onPressed: () {},
+                        title: 'JOINED',
+                        backgroundColor: Color(0xff53E40D),
+                        titleColor: Colors.white,
+                        height: 30,
+                        radius: 15,
+                      ),
+                      CustomButton(
+                        width: 120,
+                        radius: 15,
+                        onPressed: () {},
+                        title: 'DO KARMA',
+                        backgroundColor: Colors.white,
+                        titleColor: Colors.black,
+                        height: 30,
+                      ),
+                    ],
+                  )
+                ]),
+          ],
+        ),
+      ),
+    );
+  }
+}
